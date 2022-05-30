5658f558f0d199fdc149ce13fa7cef6c88fffea0
Jagad Yudha <jagadyudhaawali@gmail.com>
2022-05-30 15:37:11 +0700

    feat: add radio button components

--- pages/tes-kecemasan.js
+++ pages/tes-kecemasan.js
@@ -1,7 +1,15 @@
+import RadioButton from "@/components/radio-button";
 import React from "react";
 
 const TesKecemasan = () => {
-  return <div>TesKecemasan</div>;
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  };
+  return (
+    <main className="mx-auto">
+      <RadioButton onChange={handleChange} />
+    </main>
+  );
 };
 
 export default TesKecemasan;
