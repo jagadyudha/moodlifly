807e477bf3346df2b3e6c5bb50f53e2f88762c45
Tutut <tututanjar8@gmail.com>
2022-06-06 18:27:11 +0700

    fix: remove foreign key

--- pages/api/datatraining.js
+++ pages/api/datatraining.js
@@ -1,9 +1,7 @@
 import { supabase } from "@/lib/supabase";
 
 export default async function handler(req, res) {
-  const { data, error } = await supabase
-    .from("data_training")
-    .select("*, jenis_gangguan (kd_penyakit, nama))");
+  const { data, error } = await supabase.from("data_training").select("*");
 
   res.status(200).json(data);
 }
